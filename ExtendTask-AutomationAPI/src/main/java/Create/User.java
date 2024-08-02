a92703c8416187b3ccf4ffcbad3665e6eaeab1e4
Baraa M. Rihan <74668387+BaraaRihan@users.noreply.github.com>
2024-08-02 15:59:29 +0300

    Add files via upload

---
+++ ExtendTask-AutomationAPI/src/main/java/Create/User.java
@@ -0,0 +1,35 @@
+package Create;
+
+public class User {
+
+
+
+        private String name;
+        private String Email;
+
+        public User() {
+        }
+
+        public User(String name, String Email) {
+            this.name = name;
+            this.Email = Email;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return Email;
+        }
+
+        public void setJob(String Email) {
+            this.Email = Email;
+        }
+    }
+
+
